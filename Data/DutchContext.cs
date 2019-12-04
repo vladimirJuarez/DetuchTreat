4028b01e780088301ba7e6dc234b2f302471d332
Vladimir Juarez <vjuarez@bmvcorp.com.mx>
2019-12-03 19:01:12 -0600

    entityFrameCore some changes, entities, and configuration

---
+++ Data/DutchContext.cs
@@ -0,0 +1,29 @@
+using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DutchTreat.Data
+{
+    public class DutchContext : DbContext
+    {
+        public DutchContext(DbContextOptions<DutchContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        /*protected override void OnModelCreating(ModelBuilder builder)
+        {
+          base.OnModelCreating(builder);
+
+          builder.Entity<Product>()
+            .Property(p => p.Price)
+            .HasColumnType("decimal(18,2)");
+
+          builder.Entity<OrderItem>()
+            .Property(p => p.UnitPrice)
+            .HasColumnType("decimal(18,2)");
+        }
+      }*/
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace DutchTreat
@@ -13,14 +13,22 @@ namespace DutchTreat
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(SetupConfiguration)
+            .UseStartup<Startup>()
+            .Build();
+
+        private static void SetupConfiguration(WebHostBuilderContext ctx, IConfigurationBuilder builder)
+        {
+            // Removing the default configuration options
+            builder.Sources.Clear();
+
+            builder.AddJsonFile("config.json", false, true)
+                   .AddEnvironmentVariables();
+        }
     }
 }
