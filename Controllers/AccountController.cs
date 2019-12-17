233441454a5754dc18f2cf3d1641a0c6531278c5
Vicente Vladimir Juarez Juarez <vladimir@MacBook-Pro-de-Vicente.local>
2019-12-16 22:39:30 -0600

    identity login and logout

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,15 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
+using DutchTreat.ViewModels;
+using System.Threading.Tasks;
+using DutchTreat.Data.Entities;
+using System.Linq;
 
 namespace DutchTreat.Controllers
 {
     public class AccountController: Controller
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<StoreUser> _signInManager;
 
-        public AccountController(ILogger<AccountController> logger)
+        public AccountController(ILogger<AccountController> logger, SignInManager<StoreUser> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
 
         public IActionResult Login()
@@ -20,6 +27,37 @@ namespace DutchTreat.Controllers
             }
             
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false);
+
+                if(result.Succeeded)
+                {
+                    if(Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        return Redirect(Request.Query["ReturnUrl"].First());
+                    }
+                    else
+                    {
+                        return RedirectToAction("Shop", "App");
+                    }
+                    
+                }
+            }
+            ModelState.AddModelError("", "Failed to login");
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "App");
         }
     }
 }
